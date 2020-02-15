c5fb7276df87c0b6cdd1c3ddf103e77e4d9bbb84
Matěj Schuh <schuhmat@fit.cvut.cz>
2020-02-15 23:25:34 +0100

    Suggestion for geometry issue implemented.

--- window/middleware.py
+++ window/middleware.py
@@ -5,22 +5,34 @@ from PyQt5.QtWidgets import QWidget
 class Frame(QWidget):
     def __init__(self, master=None):
         self.master = master
+
+        # Solving geometry issue.
+        if master.first_window is None:
+            master.first_window = self
+
         super(Frame, self).__init__()
 
-    def mainloop(self):
-        # TODO No geometry
-        self.resize(200,100)
-        self.move(50,150)
-
-        self.show()
-        sys.exit(self.master.exec_())
+    def mainloop(self): # Interface solving Tkinter mainloop call on window issue.
+        self.master.mainloop()
         
 
 class Tk(QApplication):
     def __init__(self):
-        super(Tk, self).__init__(sys.argv) # TODO Real args?
+        super(Tk, self).__init__(sys.argv)
+        # Solving geometry issue.
+        self.size = None
+        self.first_window = None
 
     def geometry(self, size):
-        pass # TODO
+        self.size = size
+
+    def mainloop(self):
+        # Solving geometry issue.
+        self.first_window.resize(200,100)
+        self.first_window.move(50,150)
+
+        self.first_window.show()
+
+        sys.exit(self.exec_())
 
 
