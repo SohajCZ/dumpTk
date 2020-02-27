6230ddb2917022347faf2688be37497b3ef1729a
Matěj Schuh <schuhmat@fit.cvut.cz>
2020-02-27 20:09:53 +0100

    Returning to original look of hello world.

--- hello_world/hello_world_tkinter.py
+++ hello_world/hello_world_tkinter.py
@@ -1,10 +1,10 @@
-import middleware as tk
+import tkinter as tk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        #self.pack() # TODO 
+        self.pack()
         self.create_widgets()
 
     def create_widgets(self):
@@ -15,7 +15,7 @@ class Application(tk.Frame):
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
-        self.quit.move(100,0) # TODO return pack(side="bottom") # TODO implement
+        self.quit.pack(side="bottom")
 
     def say_hi(self):
         print("hi there, everyone!")
