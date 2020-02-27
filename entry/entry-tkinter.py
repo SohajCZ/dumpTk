2e5a90d0d5bf0593841470738d557851688519dc
Matěj Schuh <schuhmat@fit.cvut.cz>
2020-02-27 19:51:39 +0100

    Added entry example.

---
+++ entry/entry-tkinter.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+class WindowWithEntryField(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        defaultStrVar = tk.StringVar()
+        defaultStrVar.set("text")
+        self.fieldEntry = tk.Entry(self, textvariable = defaultStrVar)
+        self.fieldEntry.pack()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = WindowWithEntryField(master=root)
+    app.mainloop()
